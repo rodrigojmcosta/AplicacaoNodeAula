f32793d31de381af97cddc96f36d2b994650e7bb
Rodrigo <rodrigojosemodesto@gmail.com>
2021-09-15 21:55:09 -0300

    Fazendo os primeiros testes usando node.js

---
+++ calculator.test.js
@@ -0,0 +1,16 @@
+// import { expect, test } from '@jest/globals'; //FORA DE MÓDULO
+// import * as calculator from './calculator.js'; //FORA DE MÓDULO
+
+const calculator = require('./calculator');
+
+test("testando a soma de 1+1", () => {
+    expect(calculator.soma(1,1)).toBe(2);
+})
+
+test("testando divisão de 42/6", () => {
+    expect(calculator.sdivisao(42,6)).toBe(7);
+})
+
+test("testando a soma de 5+5", () => {
+    expect(calculator.soma(5,5)).toBe(11);
+})
